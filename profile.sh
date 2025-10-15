b7a1beef3a29b57ad5f67e9a71df46990f287fda
Plux Stahre <github@plux.se>
2025-10-15 14:52:02 +0200

    Clean up old helper stuff

--- profile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-rm /mp3/*.db
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-BIN="${SCRIPT_DIR}/build/bin/mp3build"
-"$BIN" -f
-gprof "$BIN" | less
-
--- scanbuild.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-make clean
-scan-build -analyze-headers --experimental-checks -k -V -stats -maxloop 50 make
